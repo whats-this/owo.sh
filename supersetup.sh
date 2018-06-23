70dcdc3c7b0d26443e62977fc7c6fab06185d4c0
okashi <okashi@okashi.me>
2018-06-23 08:12:31 +0200

    GitLab yeet

--- supersetup.sh
+++ supersetup.sh
@@ -3,6 +3,6 @@ trap 'exit 130' INT
 
 
 echo "Cloning git repository..."
-git clone https://github.com/whats-this/owo.sh.git /tmp/owo &> /dev/null
+git clone https://owo.codes/whats-this/owo.sh.git /tmp/owo &> /dev/null
 echo "Running setup.sh..."
 bash /tmp/owo/setup.sh
